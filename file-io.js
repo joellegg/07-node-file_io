ba247f3bbd99b905e5e96e06a3ed2c05be629f3f
joellegg <joellegg@bellsouth.net>
2017-03-03 15:29:23 -0600

    added comment for printing as a native string

--- file-io.js
+++ file-io.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 const { readFileSync } = require('fs');
+// encoding = 'utf8' returns it as a native string
+console.log(readFileSync('file-io.json', encoding = 'utf8'));
 
+//// Alternate method
 // readFile('file-io.json', (err, data) => {
 //   if (err) throw err;
 //   console.log(data.toString());
 // });
-
-console.log(readFileSync('file-io.json', encoding = 'utf8'));
